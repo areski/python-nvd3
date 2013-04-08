bbc9baba5f34cc29582378c433bd16588c07e85d
Areski Belaid <areski@gmail.com>
2013-04-08 19:13:17 +0200

    add new example

---
+++ examples01.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Examples for Python-nvd3 is a Python wrapper for NVD3 graph library.
+NVD3 is an attempt to build re-usable charts and chart components
+for d3.js without taking away the power that d3.js gives you.
+
+Project location : https://github.com/areski/python-nvd3
+"""
+
+import nvd3
+
+#Open File for test
+output_file = open('test1.html', 'w')
+output_file.write(nvd3.template_header_nvd3)
+
+type = "pieChart"
+chart = nvd3.pieChart(name=type, height=400, width=400)
+xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple"]
+ydata = [3, 4, 0, 1, 5, 7, 3]
+
+chart.add_serie(y=ydata, x=xdata)
+chart.buildhtml()
+
+output_file.write("\n\n<h2>" + type + "</h2>\n\n")
+output_file.write(chart.htmlcontent)
+
+#close Html file
+output_file.close()
