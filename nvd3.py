f1785e5969756c42023c60e1d90db4c0c6706544
Shrenik Patel <shrenik181986@gmail.com>
2013-04-09 15:18:36 +0530

    sample doc string on nvd3 classes

--- nvd3.py
+++ nvd3.py
@@ -313,7 +313,48 @@ Currently implemented nvd3 chart:
 #examples
 class lineWithFocusChart(NVD3Chart):
     """
-    usage : nvd3.lineWithFocusChart(name='lineWithFocusChart', date=True)
+    usage ::
+
+        chart = nvd3.lineWithFocusChart(name='lineWithFocusChart', date=True)
+        xdata = [1365026400000000, 1365026500000000, 1365026600000000]
+        ydata = [-6, 5, -1]
+        chart.add_serie(y=ydata, x=xdata)
+        chart.buildhtml()
+
+    js-code ::
+
+        [{ "key" : "Serie 1",
+           "values" : [
+                { "x" : "1365026400000000",
+                  "y" : -6
+                },
+                { "x" : "1365026500000000",
+                  "y" : -5
+                },
+                { "x" : "1365026600000000",
+                  "y" : -1
+                },
+              ],
+            "yAxis" : "1"
+        }]
+
+        nv.addGraph(function() {
+                var chart = nv.models.lineWithFocusChart();
+                chart.yAxis
+                    .tickFormat(d3.format(',.2f'))
+                chart.y2Axis
+                    .tickFormat(d3.format(',.2f'))
+                chart.xAxis
+                    .tickFormat(function(d) { return d3.time.format('%d %b %y')(new Date(d)) })
+                chart.x2Axis
+                    .tickFormat(function(d) { return d3.time.format('%d %b %y')(new Date(d)) })
+                d3.select('#lineWithFocusChart svg')
+                    .datum(data_lineWithFocusChart)
+                    .transition()
+                    .duration(500)
+                    .call(chart);
+            return chart;
+        });
     """
     def __init__(self, height=450, width=None, date=False, **kwargs):
         NVD3Chart.__init__(self, **kwargs)
