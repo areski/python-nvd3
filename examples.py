bdc2d050df5bfc208a720933e12c8eaa13a485bc
Shrenik Patel <shrenik181986@gmail.com>
2013-04-12 13:39:10 +0530

    add example for cumulativeLineChart

--- examples.py
+++ examples.py
@@ -103,7 +103,24 @@ kwargs = {}
 kwargs['bar'] = True
 chart.add_serie(y=ydata, x=xdata, **kwargs)
 chart.add_serie(y=ydata2, x=xdata)
-#chart.add_serie(y=ydata3, x=xdata)
+chart.buildhtml()
+
+output_file.write(chart.htmlcontent)
+#---------------------------------------
+
+type = "cumulativeLineChart"
+chart = nvd3.cumulativeLineChart(name=type, height=350, date=True)
+chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+start_time = int(time.mktime(datetime.datetime(2012, 6, 1).timetuple()) * 1000)
+
+nb_element = 100
+xdata = range(nb_element)
+xdata = map(lambda x: start_time + x * 1000000000, xdata)
+ydata = [i + random.randint(1, 10) for i in range(nb_element)]
+ydata2 = map(lambda x: x * 2, ydata)
+
+chart.add_serie(y=ydata, x=xdata)
+chart.add_serie(y=ydata2, x=xdata)
 chart.buildhtml()
 
 output_file.write(chart.htmlcontent)
--- nvd3.py
+++ nvd3.py
@@ -256,7 +256,7 @@ class NVD3Chart:
             for axis_name, a in self.axislist.iteritems():
                 self.jschart += stab(2) + "chart.%s\n" % axis_name
                 for attr, value in a.iteritems():
-                    self.jschart += stab(3) + ".%s(%s)\n" % (attr, value)
+                    self.jschart += stab(3) + ".%s(%s);\n" % (attr, value)
 
         #if self.model == 'pieChart':
         if self.width:
@@ -326,6 +326,8 @@ Currently implemented nvd3 chart:
 * stackedAreaChart
 * multiBarHorizontalChart
 * linePlusBarChart
+* cumulativeLineChart
+
 """
 
 
@@ -735,6 +737,86 @@ class linePlusBarChart(NVD3Chart):
             self.set_graph_height(height)
         if width:
             self.set_graph_width(width)
+
+
+class cumulativeLineChart(NVD3Chart):
+    """
+    usage ::
+
+        chart = nvd3.cumulativeLineChart(name='cumulativeLineChart', date=True)
+        xdata = [1365026400000000, 1365026500000000, 1365026600000000]
+        ydata = [-6, 5, -1]
+        y2data = [36, 55, 11]
+        chart.add_serie(y=ydata, x=xdata)
+        chart.add_serie(y=y2data, x=xdata)
+        chart.buildhtml()
+
+    js-code ::
+
+        data_lineWithFocusChart = [
+            {
+               "key" : "Serie 1",
+               "values" : [
+                    { "x" : "1365026400000000",
+                      "y" : -6
+                    },
+                    { "x" : "1365026500000000",
+                      "y" : -5
+                    },
+                    { "x" : "1365026600000000",
+                      "y" : -1
+                    },
+                  ],
+            },
+            {
+               "key" : "Serie 2",
+               "values" : [
+                    { "x" : "1365026400000000",
+                      "y" : 34
+                    },
+                    { "x" : "1365026500000000",
+                      "y" : 56
+                    },
+                    { "x" : "1365026600000000",
+                      "y" : 32
+                    },
+                  ],
+            }
+        ]
+
+        nv.addGraph(function() {
+            var chart = nv.models.cumulativeLineChart();
+
+            chart.xAxis
+                .tickFormat(function(d) { return d3.time.format('%d %b %Y')(new Date(d)) });
+            chart.y1Axis
+                .tickFormat(d3.format('.1%'));
+
+            d3.select('#cumulativeLineChart svg')
+                .datum(data_linePlusBarChart)
+                .transition().duration(500)
+                .attr('height', 350)
+                .call(chart);
+            return chart;
+        });
+    """
+    def __init__(self, height=450, width=None, date=False, **kwargs):
+        NVD3Chart.__init__(self, **kwargs)
+        if date:
+            self.set_axis('xAxis', format='%d %b %Y', date=True)
+            self.set_custom_tooltip_flag(True)
+        else:
+            self.set_axis('xAxis', format=".2f")
+
+        self.set_axis('yAxis', format=".1%")
+
+
+        # must have a specified height, otherwise it superimposes both chars
+        if height:
+            self.set_graph_height(height)
+        if width:
+            self.set_graph_width(width)
+
 
 #TODO: Add extensive documentation on pieChart
 #settings supported
