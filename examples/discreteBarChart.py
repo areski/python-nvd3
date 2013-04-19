026b0064989e2454b2c92c9b56639a2b2004b1db
Areski Belaid <areski@gmail.com>
2013-04-19 10:41:33 +0200

    example discreteBarChart

---
+++ examples/discreteBarChart.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Examples for Python-nvd3 is a Python wrapper for NVD3 graph library.
+NVD3 is an attempt to build re-usable charts and chart components
+for d3.js without taking away the power that d3.js gives you.
+
+Project location : https://github.com/areski/python-nvd3
+"""
+
+from nvd3 import discreteBarChart
+from nvd3 import multiBarChart
+import random
+
+#Open File for test
+output_file = open('test1.html', 'w')
+
+type = "discreteBarChart"
+chart = discreteBarChart(name=type, height=400, width=400)
+chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+xdata = ["A", "B", "C", "D", "E", "F", "G"]
+ydata = [3, 12, -10, 5, 35, -7, 2]
+
+extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
+chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
+
+chart.buildhtml()
+output_file.write(chart.htmlcontent)
+#---------------------------------------
+
+type = "multiBarChart"
+chart = multiBarChart(name=type, height=350)
+chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+nb_element = 10
+xdata = range(nb_element)
+ydata = [random.randint(1, 10) for i in range(nb_element)]
+ydata2 = map(lambda x: x * 2, ydata)
+
+extra_serie = {"tooltip": {"y_start": "", "y_end": " call"}}
+chart.add_serie(name="Count", y=ydata, x=xdata, extra=extra_serie)
+extra_serie = {"tooltip": {"y_start": "", "y_end": " min"}}
+chart.add_serie(name="Duration", y=ydata2, x=xdata, extra=extra_serie)
+chart.buildhtml()
+
+output_file.write(chart.htmlcontent)
+#---------------------------------------
+
+#close Html file
+output_file.close()
