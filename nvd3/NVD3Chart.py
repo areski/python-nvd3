59f7e93242f1b99803733257a8d50d7832152cbe
Areski Belaid <areski@gmail.com>
2013-04-23 18:20:40 +0200

    update version / fix style

--- nvd3/NVD3Chart.py
+++ nvd3/NVD3Chart.py
@@ -276,13 +276,14 @@ class NVD3Chart:
         #Create SVG div with style
         if self.width:
             if self.width[-1] != '%':
-                self.width += 'px'
-            self.style += 'width:%s;' % self.width
+                self.style += 'width:%spx;' % self.width
+            else:
+                self.style += 'width:%s;' % self.width
         if self.height:
             if self.height[-1] != '%':
-                self.height += 'px'
-            self.style += 'height:%s;' % self.height
-
+                self.style += 'height:%spx;' % self.height
+            else:
+                self.style += 'height:%s;' % self.height
         if self.style:
             self.style = 'style="%s"' % self.style
 
