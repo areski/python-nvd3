6ab50139f02a05cf3ef27981ce333a863838d1b6
Areski Belaid <areski@gmail.com>
2013-04-23 11:49:45 +0200

    update version / changelog

--- nvd3/__init__.py
+++ nvd3/__init__.py
@@ -9,7 +9,7 @@ for d3.js without taking away the power that d3.js gives you.
 Project location : https://github.com/areski/python-nvd3
 """
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 
 from lineChart import lineChart
