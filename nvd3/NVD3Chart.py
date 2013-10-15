3b56fe11e337a6adbc94514113282e24e1f948bd
Kevin Saliou <kevin@saliou.name>
2013-10-15 19:18:27 +0200

    add package.json for handling npm dependencies and bower.json for front end assets dependencies & make assets_directory variable dynamic against .bowerrc configuration file

--- nvd3/NVD3Chart.py
+++ nvd3/NVD3Chart.py
@@ -11,6 +11,7 @@ Project location : https://github.com/areski/python-nvd3
 
 from optparse import OptionParser
 from string import Template
+import os
 import json
 
 template_content_nvd3 = """
@@ -69,6 +70,9 @@ class NVD3Chart:
         * ``template_page_nvd3`` - template variable
         * ``width`` - Set graph width
         * ``x_axis_date`` - False / True
+        * ``show_legend`` - False / True
+        * ``show_labels`` - False / True
+        * ``assets_directory`` directory holding the assets (./bower_components/)
     """
     count = 0
     dateformat = '%x'
@@ -99,14 +103,21 @@ class NVD3Chart:
     x_axis_format = ''
     show_legend = True
     show_labels = True
+    assets_directory = './bower_components/'
 
     def __init__(self, **kwargs):
         """
         Constructor
         """
+
+        if not os.path.exists('../.bowerrc'):
+            f = open('.bowerrc', 'r')
+            tmp = json.load(f)
+            if "" != tmp['directory']:
+                self.assets_directory = tmp['directory']
+
         #set the model
         self.model = self.__class__.__name__
-
         #Init Data
         self.series = []
         self.axislist = {}
@@ -119,23 +130,23 @@ class NVD3Chart:
         self.color_category = kwargs.get('color_category', None)
         self.stacked = kwargs.get('stacked', False)
         self.resize = kwargs.get('resize', False)
-        self.show_legend = kwargs.get("show_legend", True)
-        self.show_labels = kwargs.get("show_labels", True)
-        self.tag_script_js = kwargs.get("tag_script_js", True)
+        self.show_legend = kwargs.get('show_legend', True)
+        self.show_labels = kwargs.get('show_labels', True)
+        self.tag_script_js = kwargs.get('tag_script_js', True)
 
         #CDN http://cdnjs.com/libraries/nvd3/ needs to make sure it's up to date
         self.header_css = [
-            '<link media="all" href="%s" type="text/css" rel="stylesheet" />\n' % h for h in
+            '<link href="%s" rel="stylesheet">\n' % h for h in
             (
-                './bower_components/nvd3/src/nv.d3.css',
+                self.assets_directory + 'nvd3/src/nv.d3.css',
             )
         ]
 
         self.header_js = [
-            '<script src="%s" type="text/javascript"></script>\n' % h for h in
+            '<script src="%s"></script>\n' % h for h in
             (
-                './bower_components/d3/d3.min.js',
-                './bower_components/nvd3/nv.d3.min.js'
+                self.assets_directory + 'd3/d3.min.js',
+                self.assets_directory + 'nvd3/nv.d3.min.js',
             )
         ]
 
@@ -171,14 +182,14 @@ class NVD3Chart:
 
         # For scatterChart shape & size fields are added in serie
         if 'shape' in kwargs or 'size' in kwargs:
-            csize = kwargs.get("size", 1)
-            cshape = kwargs.get("shape", "circle")
+            csize = kwargs.get('size', 1)
+            cshape = kwargs.get('shape', 'circle')
 
             serie = [{
-                "x": x[i],
-                "y": y,
-                "shape": cshape,
-                "size": csize[i] if isinstance(csize, list) else csize
+                'x': x[i],
+                'y': y,
+                'shape': cshape,
+                'size': csize[i] if isinstance(csize, list) else csize
             } for i, y in enumerate(y)]
         else:
             if self.model == 'pieChart':
@@ -361,7 +372,7 @@ class NVD3Chart:
 
         self.jschart = ''
         if self.tag_script_js:
-            self.jschart += '\n<script type="text/javascript">\n'
+            self.jschart += '\n<script>\n'
 
         self.jschart += stab()
 
