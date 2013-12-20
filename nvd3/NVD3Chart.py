4b0abc88cb52b9731f46bce8a812476c59636a7c
Areski Belaid <areski@gmail.com>
2013-12-20 14:26:00 +0100

    fix colors for linecharts related to https://github.com/areski/django-nvd3/issues/16

--- nvd3/NVD3Chart.py
+++ nvd3/NVD3Chart.py
@@ -219,8 +219,8 @@ class NVD3Chart:
         if 'disabled' in kwargs and kwargs['disabled']:
             data_keyvalue['disabled'] = 'true'
 
-        if 'color' in kwargs and kwargs['color']:
-            data_keyvalue['color'] = kwargs['color']
+        if 'color' in extra and extra['color']:
+            data_keyvalue['color'] = extra['color']
 
         if extra.get('date_format'):
             self.charttooltip_dateformat = extra['date_format']
