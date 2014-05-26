f85e5cd8f9325dbe6642bff767de6b2276768762
areski <areski@gmail.com>
2014-05-26 19:23:42 +0200

    update version 0.12.0

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
 git flow release start v$1
-sed -i -e "s/__version__ = '    *'/__version__ = '$1'/g" nvd3/__init__.py
+sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" nvd3/__init__.py
 #rm -rf docs/html
 #python setup.py develop
 #make docs
