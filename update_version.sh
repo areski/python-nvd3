63fb44187c64c11db8ec5dcda3880d147841177a
areski <areski@gmail.com>
2014-05-26 18:53:17 +0200

    update version script

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 git flow release start v$1
 sed -i -e "s/__version__ = '    *'/__version__ = '$1'/g" nvd3/__init__.py
 #rm -rf docs/html
-python setup.py develop
+#python setup.py develop
 #make docs
 #git commit docs nvd3/__init__.py -m "Update to version v$1"
 git commit nvd3/__init__.py -m "Update to version v$1"
