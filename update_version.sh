e7b5fe2634ffc3dbef2bc93024e01d9223fb6be2
areski <areski@gmail.com>
2014-05-27 00:47:36 +0200

    update script

--- update_version.sh
+++ update_version.sh
@@ -4,6 +4,6 @@ sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" nvd3/__init__.py
 #python setup.py develop
 #make docs
 #git commit docs nvd3/__init__.py -m "Update to version v$1"
-git commit nvd3/__init__.py -m "Update to version v$1"
+git commit -a -m "Update to version v$1"
 git flow release finish v$1
 python setup.py sdist upload -r pypi
