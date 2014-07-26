61804f96e76c78d63e5cbe2af27ff250c84f40d7
oz123 <nahumoz@gmail.com>
2014-07-26 19:54:53 +0200

    Add LinePlusBarChart

--- nvd3/linePlusBarChart.py
+++ nvd3/linePlusBarChart.py
@@ -10,6 +10,8 @@ Project location : https://github.com/areski/python-nvd3
 """
 
 from .NVD3Chart import NVD3Chart
+from jinja2 import Environment, FileSystemLoader
+import os
 
 
 class linePlusBarChart(NVD3Chart):
@@ -114,3 +116,69 @@ class linePlusBarChart(NVD3Chart):
             self.set_graph_height(height)
         if width:
             self.set_graph_width(width)
+
+
+class LinePlusBarChart(NVD3Chart):
+
+    CHART_FILENAME = "./lineplusbarchart.html"
+
+    template_environment = Environment(lstrip_blocks=True, trim_blocks=True)
+    template_environment.loader = FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
+    template_chart_nvd3 = template_environment.get_template(CHART_FILENAME)
+
+    def __init__(self, **kwargs):
+        super(LinePlusBarChart, self).__init__(**kwargs)
+        self.model = 'linePlusBarChart'
+
+        height = kwargs.get('height', 450)
+        width = kwargs.get('width', None)
+        self.yaxis1_format = kwargs.get('yaxis1_format', "function(d) { return d3.format(',f')(d) }")
+        self.yaxis2_format = kwargs.get('yaxis2_format', "function(d) { return d3.format(',f')(d) }")
+
+        if kwargs.get('x_is_date', False):
+            self.set_date_flag(True)
+            self.create_x_axis('xAxis',
+                               format=kwargs.get('x_axis_format', '%d %b %Y %H %S'),
+                               date=True)
+            self.set_custom_tooltip_flag(True)
+        else:
+            self.create_x_axis('xAxis', format=kwargs.get('x_axis_format', '.2f'))
+
+        self.create_y_axis('y1Axis', format=self.yaxis1_format, custom_format=True)
+        self.create_y_axis('y2Axis', format=self.yaxis2_format, custom_format=True)
+
+        # must have a specified height, otherwise it superimposes both chars
+        if height:
+            self.set_graph_height(height)
+        if width:
+            self.set_graph_width(width)
+
+    def buildjschart(self):
+        """
+        This only renders the template discretebarchart.html,
+        the rest of the body is renderd by calling NVD3Chart.buildhtml
+        """
+        NVD3Chart.buildjschart(self)
+
+    def buildcontent(self):
+        """Build HTML content only, no header or body tags. To be useful this
+        will usually require the attribute `juqery_on_ready` to be set which
+        will wrap the js in $(function(){<regular_js>};)
+        """
+        self.buildcontainer()
+        # if the subclass has a method buildjs this method will be
+        # called instead of the method defined here
+        # when this subclass method is entered it does call
+        # the method buildjschart defined here
+        self.buildjschart()
+        self.htmlcontent = self.template_chart_nvd3.render(chart=self)
+
+    def buildhtml(self):
+        """Build the HTML page
+        Create the htmlheader with css / js
+        Create html page
+        Add Js code for nvd3
+        """
+        self.buildcontent()
+        self.content = self.htmlcontent
+        self.htmlcontent = self.template_page_nvd3.render(chart=self)
