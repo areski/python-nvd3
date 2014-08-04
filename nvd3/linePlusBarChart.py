390a502b8761b16b67910a86609a7bc591ebb16b
oz123 <nahumoz@gmail.com>
2014-08-04 19:43:04 +0200

    Remove old class, add new class alias

--- nvd3/linePlusBarChart.py
+++ nvd3/linePlusBarChart.py
@@ -14,7 +14,8 @@ from jinja2 import Environment, FileSystemLoader
 import os
 
 
-class linePlusBarChart(NVD3Chart):
+class LinePlusBarChart(TemplateMixin, NVD3Chart):
+
     """
     A linePlusBarChart Chart is a type of chart which displays information
     as a series of data points connected by straight line segments
@@ -92,34 +93,6 @@ class linePlusBarChart(NVD3Chart):
         </script>
 
     """
-    def __init__(self, **kwargs):
-        NVD3Chart.__init__(self, **kwargs)
-        height = kwargs.get('height', 450)
-        width = kwargs.get('width', None)
-        self.yaxis1_format = kwargs.get('yaxis1_format', "function(d) { return d3.format(',f')(d) }")
-        self.yaxis2_format = kwargs.get('yaxis2_format', "function(d) { return d3.format(',f')(d) }")
-
-        if kwargs.get('x_is_date', False):
-            self.set_date_flag(True)
-            self.create_x_axis('xAxis',
-                               format=kwargs.get('x_axis_format', '%d %b %Y %H %S'),
-                               date=True)
-            self.set_custom_tooltip_flag(True)
-        else:
-            self.create_x_axis('xAxis', format=kwargs.get('x_axis_format', '.2f'))
-
-        self.create_y_axis('y1Axis', format=self.yaxis1_format, custom_format=True)
-        self.create_y_axis('y2Axis', format=self.yaxis2_format, custom_format=True)
-
-        # must have a specified height, otherwise it superimposes both chars
-        if height:
-            self.set_graph_height(height)
-        if width:
-            self.set_graph_width(width)
-
-
-class LinePlusBarChart(TemplateMixin, NVD3Chart):
-
     CHART_FILENAME = "./lineplusbarchart.html"
 
     template_environment = Environment(lstrip_blocks=True, trim_blocks=True)
@@ -157,3 +130,5 @@ class LinePlusBarChart(TemplateMixin, NVD3Chart):
         self.set_graph_height(height)
         if width:
             self.set_graph_width(width)
+
+linePlusBarChart = LinePlusBarChart
