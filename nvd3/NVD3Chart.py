472b32862ac82273d632fcb0cd1c6e22ca09f240
Anand Patil <anand.prabhakar.patil@gmail.com>
2015-03-11 21:05:30 -0700

    Further fix for global scope issue

--- nvd3/NVD3Chart.py
+++ nvd3/NVD3Chart.py
@@ -371,7 +371,7 @@ class NVD3Chart(object):
         self.htmlheader = ''
         # If the JavaScript assets have already been injected, don't bother re-sourcing them.
         global _js_initialized
-        if not _js_initialized:
+        if '_js_initialized' not in globals() or not _js_initialized:
             for css in self.header_css:
                 self.htmlheader += css
             for js in self.header_js:
