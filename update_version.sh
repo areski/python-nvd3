2acace8dcb8b0a47873806379b5307c99d07845f
Oz N Tiram <nahumoz@gmail.com>
2015-05-03 11:26:19 +0200

    Suggest to use twine instead of setup.py upload

--- update_version.sh
+++ update_version.sh
@@ -6,12 +6,12 @@
 git flow release start v$1
 sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" nvd3/__init__.py
 sed -i -e "s/version='.*'/version='$1'/g" setup.py
-#rm -rf docs/html
-#python setup.py develop
-#make docs
-#git commit docs nvd3/__init__.py -m "Update to version v$1"
-git commit -a -m "Update to version v$1"
-git flow release finish v$1
-python setup.py sdist upload -r pypi
 
+git commit -a -m "Update to version v$1"
+
+# this created the tag
+git flow release finish v$1
+
+python setup.py sdist
+twine upload dist/python-nvd3-$1.tar.gz
 git push origin develop; git push origin master; git push --tags
