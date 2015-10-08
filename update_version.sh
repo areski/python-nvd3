f96590abc0d10c22e482201595bb8daf061b5b1e
areski <areski@gmail.com>
2015-10-08 11:32:28 +0200

    remove keyword upload at the end twine command

--- update_version.sh
+++ update_version.sh
@@ -13,5 +13,5 @@ sed -i -e "s/version='.*'/version='$1'/g" setup.py
 git commit -a -m "Update to version v$1"
 git flow release finish v$1
 python setup.py sdist 
-twine upload dist/python-nvd3-$1.tar.gz upload
+twine upload dist/python-nvd3-$1.tar.gz
 git push origin develop; git push origin master; git push --tags
