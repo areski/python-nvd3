172de21531f382720268ccbb8f5df70eda540f8d
areski <areski@gmail.com>
2015-12-10 12:23:57 +0100

    Update to version v0.14.2

--- nvd3/__init__.py
+++ nvd3/__init__.py
@@ -9,7 +9,7 @@ for d3.js without taking away the power that d3.js gives you.
 Project location : https://github.com/areski/python-nvd3
 """
 
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 __all__ = ['lineChart', 'pieChart', 'lineWithFocusChart',
            'stackedAreaChart', 'multiBarHorizontalChart',
            'linePlusBarChart', 'cumulativeLineChart',
