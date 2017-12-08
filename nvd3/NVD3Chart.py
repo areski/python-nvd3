1f3d00df3a86c490fae35b30da9c7b612face81f
Cyd La Luz <quetzaluz@gmail.com>
2017-12-08 10:25:24 -0800

    Add xAxis_staggerLabel option implmented similarly to xAxis_rotateLabel

--- nvd3/NVD3Chart.py
+++ nvd3/NVD3Chart.py
@@ -76,6 +76,7 @@ class NVD3Chart(object):
         :keyword: **focus_enable** - default - ``False``
         :keyword: **resize** - define - ``False``
         :keyword: **xAxis_rotateLabel** - default - ``0``
+        :keyword: **xAxis_staggerLabel** - default - ``False``
         :keyword: **show_controls** - default - ``True``
         :keyword: **show_legend** - default - ``True``
         :keyword: **show_labels** - default - ``True``
@@ -128,6 +129,7 @@ class NVD3Chart(object):
         self.focus_enable = kwargs.get('focus_enable', False)
         self.resize = kwargs.get('resize', False)
         self.xAxis_rotateLabel = kwargs.get('xAxis_rotateLabel', 0)
+        self.xAxis_staggerLabel = kwargs.get('xAxis_staggerLabel', False)
         self.show_controls = kwargs.get('show_controls', True)
         self.show_legend = kwargs.get('show_legend', True)
         self.show_labels = kwargs.get('show_labels', True)
