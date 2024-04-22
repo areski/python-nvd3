456c24faf63866bf624718859e4836572167b0d8
areski <areski@gmail.com>
2024-04-22 09:49:58 +0200

    Update to version v0.16.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='python-nvd3',
-    version='0.14.0',
+    version='0.16.0',
     description="Python NVD3 - Chart Library for d3.js",
     long_description=readme + '\n\n' + history,
     keywords='plot, graph, nvd3, d3',
